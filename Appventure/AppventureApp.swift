9cf22e1ce2e93328c5c12aea95458f39c3c6d5be
Woohyun Jo <bluewing1004@gmail.com>
2025-04-24 22:49:52 +0900

    Initial Commit

---
+++ Appventure/AppventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppventureApp.swift
+//  Appventure
+//
+//  Created by 조우현 on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AppventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appventure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appventure
+//
+//  Created by 조우현 on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

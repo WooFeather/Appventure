1cd50a9b5284d4a4ac6d577c4dcc9bbd9b082f44
Woohyun Jo <bluewing1004@gmail.com>
2025-04-24 23:01:34 +0900

    [Feat] DTO 및 Entity 구성

---
+++ Appventure/Network/DTO/AppInfoDTO.swift
@@ -0,0 +1,80 @@
+//
+//  AppInfoDTO.swift
+//  Appventure
+//
+//  Created by 조우현 on 4/24/25.
+//
+
+import Foundation
+
+// MARK: - AppInfoDTO
+struct AppInfoDTO: Codable {
+    let resultCount: Int
+    let results: [InfoResultDTO]
+}
+
+// MARK: - InfoResultDTO
+struct InfoResultDTO: Codable {
+    let isGameCenterEnabled: Bool
+    let features: [String]
+    let supportedDevices: [String]
+    let advisories: [String]
+    let kind: String
+    let artworkUrl512: String
+    let screenshotUrls: [String]
+    let artistViewURL: String
+    let artworkUrl60, artworkUrl100: String
+    let ipadScreenshotUrls: [String]
+    let appletvScreenshotUrls: [String]
+    let artistID: Int
+    let artistName: String
+    let genres: [String]
+    let price: Int
+    let bundleID: String
+    let currentVersionReleaseDate: String
+    let sellerName: String
+    let genreIDS: [String]
+    let releaseNotes, trackName: String
+    let trackID: Int
+    let isVppDeviceBasedLicensingEnabled: Bool
+    let primaryGenreName: String
+    let primaryGenreID: Int
+    let version: String
+    let wrapperType: String
+    let currency: String
+    let description: String
+    let sellerURL: String?
+    let minimumOSVersion: String
+    let languageCodesISO2A: [String]
+    let fileSizeBytes: String
+    let formattedPrice: String
+    let userRatingCountForCurrentVersion: Int
+    let trackContentRating: String
+    let trackCensoredName: String
+    let averageUserRatingForCurrentVersion, averageUserRating: Double
+    let trackViewURL: String
+    let contentAdvisoryRating: String
+    let releaseDate: String
+    let userRatingCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case isGameCenterEnabled, features, supportedDevices, advisories, kind, artworkUrl512, screenshotUrls
+        case artistViewURL = "artistViewUrl"
+        case artworkUrl60, artworkUrl100, ipadScreenshotUrls, appletvScreenshotUrls
+        case artistID = "artistId"
+        case artistName, genres, price
+        case bundleID = "bundleId"
+        case currentVersionReleaseDate, sellerName
+        case genreIDS = "genreIds"
+        case releaseNotes, trackName
+        case trackID = "trackId"
+        case isVppDeviceBasedLicensingEnabled, primaryGenreName
+        case primaryGenreID = "primaryGenreId"
+        case version, wrapperType, currency, description
+        case sellerURL = "sellerUrl"
+        case minimumOSVersion = "minimumOsVersion"
+        case languageCodesISO2A, fileSizeBytes, formattedPrice, userRatingCountForCurrentVersion, trackContentRating, trackCensoredName, averageUserRatingForCurrentVersion, averageUserRating
+        case trackViewURL = "trackViewUrl"
+        case contentAdvisoryRating, releaseDate, userRatingCount
+    }
+}
---
+++ Appventure/Network/Entity/AppInfoEntity.swift
@@ -0,0 +1,57 @@
+//
+//  AppInfoEntity.swift
+//  Appventure
+//
+//  Created by 조우현 on 4/24/25.
+//
+
+import Foundation
+
+struct AppInfoEntity {
+    let resultCount: Int
+    let results: [InfoResultEntity]
+}
+
+struct InfoResultEntity {
+    let id: Int // trackID
+    
+    let name: String // trackName
+    let iconUrl: String // artworkUrl100
+    let genres: [String]
+    let primaryGenreName: String
+    let minimumVersion: String // minimumOsVersion
+    let corpName: String // artistName
+    
+    let screenShotsUrls: [String]
+    let age: String // trackContentRating
+    let languages: [String] // languageCodesISO2A
+    
+    let releaseNotes: String
+    let currentReleaseDate: String // currentVersionReleaseDate
+    let version: String
+}
+
+extension AppInfoDTO {
+    func toEntity() -> AppInfoEntity {
+        return AppInfoEntity(
+            resultCount: self.resultCount,
+            results: self.results.map {
+                InfoResultEntity(
+                    id: $0.trackID,
+                    name: $0.trackName,
+                    iconUrl: $0.artworkUrl100,
+                    genres: $0.genres,
+                    primaryGenreName: $0.primaryGenreName,
+                    minimumVersion: $0.minimumOSVersion,
+                    corpName: $0.artistName,
+                    screenShotsUrls: $0.screenshotUrls,
+                    age: $0.trackContentRating,
+                    languages: $0.languageCodesISO2A,
+                    releaseNotes: $0.releaseNotes,
+                    currentReleaseDate: $0.currentVersionReleaseDate,
+                    version: $0.version
+                )
+            }
+        )
+    }
+}
